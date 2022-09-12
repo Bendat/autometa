e75f36dfc0cceaa4e639df510a5a05c21af2ce3e
Ben Aherne <Bendat@users.noreply.github.com>
2022-09-12 15:54:02 +0100

    fixed documentation deployment (again... again)

--- packages/documentation/docusaurus.config.js
+++ packages/documentation/docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/Bendat/jest-automation',
+          editUrl: 'https://github.com/Bendat/automaton',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
