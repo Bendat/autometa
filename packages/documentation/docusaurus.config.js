efad143641e51f4dabcbd4f7e6d6ab62178e3c8f
Ben Aherne <Bendat@users.noreply.github.com>
2022-09-14 16:51:17 +0100

    Update docusaurus.config.js

--- packages/documentation/docusaurus.config.js
+++ packages/documentation/docusaurus.config.js
@@ -65,10 +65,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
