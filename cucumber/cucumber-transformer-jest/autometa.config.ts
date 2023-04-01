2233f0c9d8c6cbd6b84deadc74ad6f8d0b9a654d
Ben Aherne <Bendat@users.noreply.github.com>
2023-04-01 18:28:10 +0100

    refactor jest transformer

--- cucumber/cucumber-transformer-jest/autometa.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from "@config/config-manager";
-
-defineConfig({
-  globalsRoot: "globals",
-});
--- cucumber/cucumber-transformer-jest/index.ts
+++ cucumber/cucumber-transformer-jest/index.ts
@@ -1,1 +1,40 @@
-export * from "./src";
+import crypto from "crypto";
+
+import { transform } from "@babel/core";
+import type { Config } from "@jest/types";
+import jestPreset from "babel-preset-jest";
+
+export let canInstrument: false;
+export const getCacheKey = (
+  fileData: crypto.BinaryLike,
+  filename: crypto.BinaryLike,
+  configString: crypto.BinaryLike,
+  { instrument }: { instrument?: crypto.BinaryLike }
+) =>
+  crypto
+    .createHash("md5")
+    .update("\0", "utf8")
+    .update(fileData)
+    .update("\0", "utf8")
+    .update(filename)
+    .update("\0", "utf8")
+    .update(configString)
+    .update("\0", "utf8")
+    .update("\0", "utf8")
+    .update(instrument ? "instrument" : "")
+    .digest("hex");
+
+export function process(src: string, filePath: string, jestConfig: Config.ProjectConfig) {
+  const testFile = `
+const { Feature } = require('@cucumber/cucumber-runner');            
+Feature(${filePath})
+`;
+
+  const featureFile = transform(testFile, {
+    filename: filePath,
+    presets: [jestPreset],
+    root: jestConfig.cwd,
+  });
+
+  return featureFile ? featureFile.code : src;
+}
