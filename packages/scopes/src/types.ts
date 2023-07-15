e9969edf4a8a7f22f71309bd5975084c15ef7756
Ben Aherne <Bendat@users.noreply.github.com>
2023-07-15 14:30:52 +0100

    feat(scopes): infer argument types from step text
    It is now possible for step callbacks to automatically infer their argument types based on the step text and it's expressions.
    
    i.e for the step expression
    `Given('a cart with {int} {word}s`, (a, b, c)=>{})`
    
    `a` will automatically be inferred as a number, `b` as a string and `c` as an `App`

--- packages/scopes/src/types.ts
+++ packages/scopes/src/types.ts
@@ -1,6 +1,66 @@
+import { App } from "@autometa/app";
+import { NeverDataTable } from "@autometa/gherkin";
+
 export type FeatureAction = () => void;
 export type RuleAction = () => void;
 export type ScenarioAction = () => void;
 export type StepText = string | RegExp;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type HookAction = (...args: any[]) => unknown | Promise<unknown>;
+export type RawStepAction = (...args: unknown[]) => unknown | Promise<unknown>;
+export type HookAction = (...args: HookArguments) => unknown | Promise<unknown>;
+
+export type StepArguments = [...unknown[], App] | [App];
+
+export type InferredArguments<T extends StepArguments = [App]> = T extends [
+  ...infer THead,
+  infer TTail
+]
+  ? TTail extends App
+    ? [...THead, App]
+    : "The last argument passed must be type 'App'"
+  : [App];
+export type HookArguments = [App];
+
+export interface Types {
+  //   [key: string]: unknown;
+  string: string;
+  number: number;
+  boolean: boolean;
+  bool: boolean;
+  date: Date;
+}
+
+interface ExpressionFn<TAccumulator extends unknown[], TTable> {
+  (...args: AccumulatorWithKnownTableTable<TAccumulator, TTable>):
+    | unknown
+    | Promise<unknown>;
+}
+
+type AccumulatorWithKnownTableTable<TAcc, TTable> = TAcc extends [
+  ...infer THead,
+  infer TTail
+]
+  ? TTable extends NeverDataTable
+    ? [...THead, TTail]
+    : [...THead, TTable, TTail]
+  : never;
+
+type ErrorString<TArg extends string> =
+  `unknown expression type [${TArg}]. Please update your Types interface to map [${TArg}] to some other type..`;
+  
+export type StepWithExpressionArgs<
+  T,
+  TTable,
+  TAccumulator extends unknown[] = []
+> = T extends `${infer _prefix}{${infer TArg}}${infer TRemainder}`
+  ? TArg extends keyof Types
+    ? StepWithExpressionArgs<TRemainder, TTable, [...TAccumulator, Types[TArg]]>
+    : StepWithExpressionArgs<`unknown expression ${TArg}`, TTable, TAccumulator>
+  : T extends `unknown expression ${infer TArg}`
+  ? ExpressionFn<[...TAccumulator, ErrorString<TArg>], TTable>
+  : ExpressionFn<[...TAccumulator, App], TTable>;
+
+export type StepAction<T extends string, TTable> = StepWithExpressionArgs<
+  T,
+  TTable
+>;
