57dbdbec454078f9914668b7a27c82f0577ce5a8
benaherne-hw <ben.aherne@hostelworld.com>
2023-09-21 18:50:01 +0100

    chore: bump runner
    Trying to fix issue with old code being published

--- packages/autometa/src/events.ts
+++ packages/autometa/src/events.ts
@@ -2,6 +2,7 @@ import { TestEventEmitter } from "@autometa/events";
 import { CONFIG } from "./config";
 export * from "@autometa/events";
 import { GroupLogEvents } from "./event-logger";
+
 export function makeTestEmitter(opts: { groupLogger: boolean }) {
   const events = CONFIG.current.events;
   const emitter = new TestEventEmitter();
@@ -11,6 +12,7 @@ export function makeTestEmitter(opts: { groupLogger: boolean }) {
   if (!events) {
     return emitter;
   }
+
   for (const event of events) {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const listener = require(event);
