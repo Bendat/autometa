176e200b8e6de37713aaf6765f159b357570af7a
benaherne-hw <ben.aherne@hostelworld.com>
2023-10-15 01:18:20 +0100

    fix: event import strings should work for local or node_modules

--- packages/autometa/src/events.ts
+++ packages/autometa/src/events.ts
@@ -2,7 +2,7 @@ import { TestEventEmitter } from "@autometa/events";
 import { CONFIG } from "./config";
 export * from "@autometa/events";
 import { GroupLogEvents } from "./event-logger";
-
+import p from "path";
 export function makeTestEmitter(opts: { groupLogger: boolean }) {
   const events = CONFIG.current.events;
   const emitter = new TestEventEmitter();
@@ -14,10 +14,28 @@ export function makeTestEmitter(opts: { groupLogger: boolean }) {
   }
 
   for (const event of events) {
+    const isLocal = isRelativePath(event) || isAbsolutePath(event);
+    if (isLocal) {
+      const uriRoot = process.cwd();
+      const uri = p.join(uriRoot, event);
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const listener = require(uri);
+      emitter.load(listener);
+      return emitter;
+    }
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const listener = require(event);
     emitter.load(listener);
+    return emitter;
   }
 
   return emitter;
+}
+
+function isRelativePath(path: string) {
+  return path.startsWith(".");
+}
+
+function isAbsolutePath(path: string) {
+  return p.isAbsolute(path);
 }
